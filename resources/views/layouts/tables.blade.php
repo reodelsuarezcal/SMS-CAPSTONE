ec4f3d260827cdc0b59ccd4408b03e87991e1cab
ImLiXun17 <maurenecayao@gmail.com>
2024-10-03 23:18:52 +0800

    change-readme

--- resources/views/layouts/tables.blade.php
+++ resources/views/layouts/tables.blade.php
@@ -186,7 +186,6 @@
               <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ms-1"></i></span>
             </div>
           </footer>
-          <table id="patientTable" style="display:none;">
         </div>
       </div>
     </div>
