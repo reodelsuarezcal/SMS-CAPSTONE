fcf80714a0b6b08e5f30fb9f378e8f8799e717a9
ImLiXun17 <maurenecayao@gmail.com>
2024-11-15 16:50:50 +0800

    new-commit

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-
